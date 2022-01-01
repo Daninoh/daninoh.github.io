eb6426d570282a935ed4ed6d4dce4d07f32d878d
Daninoh <dnoh@andrew.cmu.edu>
2021-12-31 21:02:47 -0500

    up

--- javascript/script.js
+++ javascript/script.js
@@ -12,6 +12,7 @@ document.getElementById("loadanim").style.display = "block";
 var slideIndex = 1;
 showSlides(slideIndex);
 
+
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
