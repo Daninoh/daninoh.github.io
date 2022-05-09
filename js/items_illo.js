7a0cce66b88e60bd24e02c132b476ea575df3986
Daninoh <dnoh@andrew.cmu.edu>
2022-05-09 18:19:00 -0400

    illo to illustration

--- js/items_illo.js
+++ js/items_illo.js
@@ -1,32 +1,31 @@
-const posts = []
+const posts = [];
 
 const images = [
-    'img/illo/wes.jpg',
-    'img/illo/asianglow.png',
-    'img/illo/airport.png',
-    'img/illo/afloat.png',
-    'img/illo/thebarista.png',
-    'img/illo/pittsburgh.png',
-    'img/illo/readmylips.png',
-    'img/illo/pancake.png',
-    'img/illo/bunny.png',
-    'img/illo/avoidburnout.png',
-    'img/illo/faucet.png',
-    'img/illo/thefashionista.png',
-    'img/illo/fantasist.jpg',
-    'img/illo/pool.png',
-    'img/illo/airplane.png',
-]
+  "img/illustrations/wes.jpg",
+  "img/illustrations/asianglow.png",
+  "img/illustrations/airport.png",
+  "img/illustrations/afloat.png",
+  "img/illustrations/thebarista.png",
+  "img/illustrations/pittsburgh.png",
+  "img/illustrations/readmylips.png",
+  "img/illustrations/pancake.png",
+  "img/illustrations/bunny.png",
+  "img/illustrations/avoidburnout.png",
+  "img/illustrations/faucet.png",
+  "img/illustrations/thefashionista.png",
+  "img/illustrations/fantasist.jpg",
+  "img/illustrations/pool.png",
+  "img/illustrations/airplane.png",
+];
 
 let imageIndex = 0;
 
-for(let i = 1; i <= images.length; i++){
-    let item = {
-        id: i,
-        image: images[imageIndex]
-    }
-    posts.push(item);
-    imageIndex++;
-    if (imageIndex > images.length - 1) imageIndex = 0;
+for (let i = 1; i <= images.length; i++) {
+  let item = {
+    id: i,
+    image: images[imageIndex],
+  };
+  posts.push(item);
+  imageIndex++;
+  if (imageIndex > images.length - 1) imageIndex = 0;
 }
-
